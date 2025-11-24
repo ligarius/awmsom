9f15d412347b605541192559076cd68a025a0e61
ligarius <ligarius@gmail.com>
2025-11-24 08:41:21 -0300

    Add integration connector invocation and tests

--- src/modules/integrations/integrations.module.ts
+++ src/modules/integrations/integrations.module.ts
@@ -7,11 +7,13 @@ import { IntegrationsService } from './integrations.service';
 import { QueuesModule } from '../../infrastructure/queues/queues.module';
 import { IntegrationsProcessor } from './integrations.processor';
 import { PaginationService } from '../../common/pagination/pagination.service';
+import { ErpConnectorService } from '../integration/services/erp-connector.service';
+import { TmsConnectorService } from '../integration/services/tms-connector.service';
 
 @Module({
   imports: [PrismaModule, AuditModule, QueuesModule, BullModule.registerQueue({ name: 'integration-jobs-queue' })],
   controllers: [IntegrationsController],
-  providers: [IntegrationsService, IntegrationsProcessor, PaginationService],
+  providers: [IntegrationsService, IntegrationsProcessor, PaginationService, ErpConnectorService, TmsConnectorService],
   exports: [IntegrationsService],
 })
 export class IntegrationsModule {}
